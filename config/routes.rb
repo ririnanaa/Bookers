7a258bf2fb592e87b931b6d413a0b93be5c2639e
rinaS <158763752+ririnanaa@users.noreply.github.com>
2024-03-11 09:37:14 +0000

    bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  get '/' =>"homes#top"
+  root 'homes#top'
   get 'books' => "books#index"
   post 'books' => "books#create"
   get 'books/:id' => "books#show",as: "bookers"
